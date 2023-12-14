facc9ef6e665497db2925256d42238cb09971e61
Yam <102765765+Yamakadzi@users.noreply.github.com>
2023-12-14 06:42:44 +0300

    kursach

---
+++ ProductAccounting/ProductAccounting/RegistrationWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ProductAccounting.Data;
+using ProductAccounting.Models;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProductAccounting
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            using var context = new MarketContext();
+            var user = context.Users.FirstOrDefault(u => u.Login == LoginTextBox.Text || u.Email == EmailTextBox.Text);
+            try
+            {
+                if (string.IsNullOrEmpty(LoginTextBox.Text) || string.IsNullOrEmpty(EmailTextBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+                {
+                    MessageBox.Show("Заполните все поля данных");
+                }
+                else
+                {
+                    if (user == null)
+                    {
+                        // LoginTextBox != null;
+                        User user1 = new User();
+                        user1.Login = LoginTextBox.Text;
+                        user1.Email = EmailTextBox.Text;
+                        user1.Password = PasswordBox.Password;
+                       
+
+                        context.Users.Add(user1);
+                        context.SaveChanges();
+                        MessageBox.Show("Вы зарегистрировались");
+
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь с таким логином или почтой существует, придумайте другой");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($" Данные введены неправильно {ex.Message.ToString()}");
+            }
+        }
+    }
+}
